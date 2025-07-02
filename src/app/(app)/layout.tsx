86342d2f7b64461279aa4eecf711d5749bfe6e96
Florante Catapang <florantecatapang6@gmail.com>
2025-07-02 02:13:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { type ReactNode } from "react"
 import { AppHeader } from "@/components/app-header"
+import { UserProvider } from "@/hooks/use-user"
 
 export default function AppLayout({
   children,
@@ -9,9 +10,11 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <AppHeader />
-      <main className="flex-1 p-4 sm:p-6">{children}</main>
-    </div>
+    <UserProvider>
+      <div className="flex min-h-screen flex-col bg-background">
+        <AppHeader />
+        <main className="flex-1 p-4 sm:p-6">{children}</main>
+      </div>
+    </UserProvider>
   )
 }
