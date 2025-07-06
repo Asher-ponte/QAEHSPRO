9b29fc50350a253f2d10dded536a69ff51a2bdbe
Florante Catapang <florantecatapang6@gmail.com>
2025-07-06 11:34:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,5 +1,3 @@
 // This file is a placeholder to resolve a Next.js build conflict.
 // The actual page lives in /src/app/(app)/admin/users/page.tsx to ensure it receives the correct app layout.
-export {};
-
-    
+export default null;
