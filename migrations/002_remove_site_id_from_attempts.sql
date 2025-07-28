7d5b98ae81cc574e52eda3344cd873f1af253fc8
Florante Catapang <florantecatapang6@gmail.com>
2025-07-28 02:57:33 +0000

    site id still in the api insert

--- migrations/002_remove_site_id_from_attempts.sql
+++
@@ -1,6 +0,0 @@
--- This migration removes the site_id column from the final_assessment_attempts table
--- as it was redundant. The site can be determined from the course_id.
--- This file should be run against your MySQL database.
-
-ALTER TABLE final_assessment_attempts
-DROP COLUMN site_id;
