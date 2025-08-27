1d7fe2e1850241a9acffb14da21b0fe6ae0d0f70
Florante Catapang <florantecatapang6@gmail.com>
2025-08-27 07:59:51 +0000

    Finished Step #2 - "preparer" ERROR ERROR: build step 2 "us-central1-doc

--- src/next.config.ts
+++ src/next.config.ts
@@ -28,30 +28,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // This is required for packages with native Node.js dependencies.
-    if (isServer) {
-      // bcrypt is a native dependency, keep it here.
-      // TensorFlow and MediaPipe dependencies can be externalized on the server
-      // to prevent bundling issues that can lead to console errors.
-      config.externals.push('@tensorflow/tfjs', '@tensorflow-models/face-landmarks-detection', '@mediapipe/tasks-vision');
-    }
-    return config;
-  },
-  devIndicators: {
-    onLog: (log) => {
-      const messagesToIgnore = [
-          'Created TensorFlow Lite XNNPACK delegate for CPU',
-          'Sets FaceBlendshapesGraph acceleration to xnnpack by default',
-          'OpenGL error checking is disabled'
-      ];
-
-      if (messagesToIgnore.some(msg => log.message.includes(msg))) {
-        return false;
-      }
-      return true;
-    },
-  }
 };
 
 export default nextConfig;
